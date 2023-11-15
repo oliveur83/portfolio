66ddfe2639aa301780b4b6834dbadcd0bf573405
oliveur83 <tomolivierpro@gmail.com>
2023-11-15 13:42:55 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './style_add.css']
 })
 export class AppComponent {
   imagePath: SafeResourceUrl;
