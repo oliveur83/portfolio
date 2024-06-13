d53162ddaa71605dae6def8e5a72ab1b7945fdbb
oliveur83 <tomolivierpro@gmail.com>
2024-06-13 16:24:17 +0200

    gestion de route pour le reload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfilComponent } from  './normal/profil/profil.component';
+import { ProfilComponent } from './normal/profil/profil.component';
 import { ContactComponent } from './normal/contact/contact.component';
 import { PrincipalComponent } from './normal/principal/principal.component';
 import { ProjetComponent } from './normal/projet/projet.component';
-import {ProfilComponentdev} from './dev/profil/profildev.component'
+import { ProfilComponentdev } from './dev/profil/profildev.component';
 import { JeuDeLoieComponent } from './normal/projet/jeu-de-loie/jeu-de-loie.component';
 import { JeuDeDameComponent } from './normal/projet/jeu-de-dame/jeu-de-dame.component';
 import { HandtrackingComponent } from './normal/projet/handtracking/handtracking.component';
@@ -13,30 +13,24 @@ import { EDTComponent } from './normal/projet/edt/edt.component';
 import { OXIComponent } from './normal/projet/oxi/oxi.component';
 import { AngularVuejsComponent } from './normal/projet/angular-vuejs/angular-vuejs.component';
 import { ChenilleComponent } from './normal/projet/chenille/chenille.component';
-import {PageProfilComponent} from "./page-profil/page-profil.component"
+import { PageProfilComponent } from './page-profil/page-profil.component';
+
 const routes: Routes = [
-{ path: 'dev/profil', component: ProfilComponentdev }, // Assurez-vous que le composant est correct
-
-{path: 'objectif',component:PageProfilComponent },
-
-
-  {path: 'profil',component: ProfilComponent },
-  {path: 'contact',component: ContactComponent },
-  {path: 'projet',component: ProjetComponent },
-  {path: 'principal',component: PrincipalComponent },
-  {path: 'jeu-de-loie',component: JeuDeLoieComponent },
-  {path: 'jeu-de-dame',component: JeuDeDameComponent },
-  
-  {path: 'Emploi_du_temps',component: EDTComponent },
-  {path: 'Gestion_de_camping',component: GestionCampingComponent },
-  {path: 'Application_universitaire_Vuejs_&_angular',component: AngularVuejsComponent },
-  {path: 'Application_pour_OXI',component: OXIComponent },
-  {path: 'Handtracking',component: HandtrackingComponent },
-  {path: 'chenille',component: ChenilleComponent },
-
-  {
-  path:'',redirectTo:'principal',pathMatch:'full'
-}
+  { path: 'dev/profil', component: ProfilComponentdev },
+  { path: 'objectif', component: PageProfilComponent },
+  { path: 'profil', component: ProfilComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'projet', component: ProjetComponent },
+  { path: 'principal', component: PrincipalComponent },
+  { path: 'jeu-de-loie', component: JeuDeLoieComponent },
+  { path: 'jeu-de-dame', component: JeuDeDameComponent },
+  { path: 'Emploi_du_temps', component: EDTComponent },
+  { path: 'Gestion_de_camping', component: GestionCampingComponent },
+  { path: 'Application_universitaire_Vuejs_&_angular', component: AngularVuejsComponent },
+  { path: 'Application_pour_OXI', component: OXIComponent },
+  { path: 'Handtracking', component: HandtrackingComponent },
+  { path: 'chenille', component: ChenilleComponent },
+  { path: '', redirectTo: 'principal', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,90 +1,82 @@
-import { Component,ElementRef } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
 import { logo } from './logo';
 import { DataService } from './date.service';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css', './style_add.css']
 })
-
 export class AppComponent {
   imagePath: SafeResourceUrl;
-  
   isDevMode: boolean = false;
   modedevfond: boolean = false;
-  constructor( private dataService: DataService, private elementRef: ElementRef,private sanitizer: DomSanitizer, private router: Router, private activatedRoute: ActivatedRoute) {
+
+  constructor(
+    private dataService: DataService,
+    private elementRef: ElementRef,
+    private sanitizer: DomSanitizer,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.imagePath = '';
-    
-   
   }
-  
+
   ngAfterViewInit() {
     const container = this.elementRef.nativeElement.querySelector('#container');
-
     const threeJSUtils = new logo(container);
     threeJSUtils.init();
   }
+
   ngOnInit() {
-    
     this.imagePath = this.sanitizer.bypassSecurityTrustResourceUrl('assets/logo_olicode.png');
-  }
-  contact(){
-    this.router.navigate(['/contact']);
-       
-  }
-  profil(){
     
-    this.router.navigate(['/profil']);
-       
+    // Redirection vers 'principal' si l'URL actuelle est vide ou '/'
+    if (this.router.url === '/' || this.router.url === '') {
+      this.router.navigate(['/principal']);
+    }
   }
+
+  contact() {
+    this.router.navigate(['/contact']);
+  }
+
+  profil() {
+    this.router.navigate(['/profil']);
+  }
+
   modedev() {
-  
     this.updateRoute();
   }
 
   updateRoute() {
     var currentRouteservice = this.dataService.getSharedValue();
- 
     var currentRoute = this.activatedRoute.snapshot.firstChild?.routeConfig?.path || 'principal';
     var queryParams = {
       parametre1: currentRoute,
-      
     };
-    if (currentRoute!==currentRouteservice)
-    {
+
+    if (currentRoute !== currentRouteservice) {
       this.dataService.setSharedValue(currentRoute);
     }
-    console.log("avant changement ",currentRoute,currentRouteservice)
-    // Si isDevMode est true, ajoutez le préfixe 'dev/' ; sinon, retirez-le.
-    const newRoute2 = this.isDevMode ? `dev/profil` : currentRouteservice ;
 
-    document.body.style.backgroundColor = this.isDevMode ? "rgb(42, 48, 52)" :"rgb(196, 228, 244)";
-   
+    const newRoute2 = this.isDevMode ? `dev/profil` : currentRouteservice;
+    document.body.style.backgroundColor = this.isDevMode ? "rgb(42, 48, 52)" : "rgb(196, 228, 244)";
     this.router.navigate([newRoute2], { queryParams });
   }
-  
-  principal(){
-   
+
+  principal() {
     this.router.navigate(['/principal']);
-       
   }
-  
-  projet(){
-    
+
+  projet() {
     this.router.navigate(['/projet']);
-       
   }
-Test(){
-    console.log("je suis la ")
+
+  Test() {
+    console.log("je suis la");
     this.router.navigate(['/test']);
-       
   }
 }
-
-   //const currentRoute = this.activatedRoute.snapshot.firstChild?.routeConfig?.path || 'principal';
-
